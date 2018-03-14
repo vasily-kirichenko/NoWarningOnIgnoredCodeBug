eb6a09c6abd52e77dfaf4a5b234ec424007a50f6
Vasily Kirichenko <vaskir@gmail.com>
2018-03-14 22:09:28 +0300

    init

---
+++ NoWarningOnIgnoredCodeBug/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace NoWarningOnIgnoredCodeBug.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("NoWarningOnIgnoredCodeBug")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("NoWarningOnIgnoredCodeBug")>]
+[<assembly: AssemblyCopyright("Copyright ©  $year$")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("1597E21C-CB5E-4816-9DAC-A31E6CE10F26")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ NoWarningOnIgnoredCodeBug/Program.fs
@@ -0,0 +1,29 @@
+﻿open Akka
+open Akka.Actor
+open Akka.Streams
+open Akka.Streams.Dsl
+open Akkling
+open Akkling.Streams
+open Akkling.Streams.Operators
+open System
+
+type IUser = interface end
+type IHashtagEntity = interface end
+
+type ITweet = 
+    abstract Hashtags: IHashtagEntity list
+    abstract CreatedBy: IUser
+
+[<EntryPoint>]
+let main _ = 
+    use system = ActorSystem.Create("test")
+    let mat = system.Materializer()
+    
+    GraphDsl.Create (fun b ->
+        let broadcast = b.Add(Broadcast<ITweet>(2))
+        ClosedShape.Instance
+    )
+    |> RunnableGraph.FromGraph
+    |> Graph.run mat
+    
+    0
